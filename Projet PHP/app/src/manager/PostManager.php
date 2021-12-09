59e01da93e887dfb85d900ef85fb08da9033b64f
ValentinR01 <vale.vr@icloud.com>
2021-12-09 13:12:06 +0100

    Udate

--- Projet PHP/app/src/manager/PostManager.php
+++ Projet PHP/app/src/manager/PostManager.php
@@ -22,16 +22,16 @@ class PostManager extends BaseManager
     {
         $query = $this->pdo->query('SELECT * FROM' . PDOFactory::DATABASE . '.posts');
         $query->execute();
-       // $query->setFetchMode(\PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, //TODO);
+        $query->setFetchMode(\PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Entity\Post');
         return $query->fetchAll();
     }
-/*
-    public function getPostByID(int $id)
+
+    public function getPostByID(int $id): Post
     {
         $query = $this->pdo->prepare('SELECT * FROM' . PDOFactory::DATABASE . '.posts' . ' ' . 'WHERE id = :id');
         $query->bindvalue(':id', $id, \PDO::PARAM_INT);
         $query->execute();
-        $query-> setFetchMode(\PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, //TODO);
+        $query-> setFetchMode(\PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Entity\Post');
         return $query->fetch();
     }
 
@@ -42,26 +42,29 @@ class PostManager extends BaseManager
         return $query->execute();
     }
 
-    //TODO
-    public function createPost(): bool
+
+    public function createPost(Post $post): bool
     {
-        $query = $this->pdo->prepare('DELETE FROM' . PDOFactory::DATABASE . '.posts' . ' ' . 'WHERE id = :id');
-        $query->bindvalue(':id', $id, \PDO::PARAM_INT);
+        $query = $this->pdo->prepare('INSERT INTO' . PDOFactory::DATABASE . '.posts (title, content, publish_date, userId) VALUES (:title, :content, :publish_date, :userId)');
+        $query->bindValue(:title, post->getTitle(), PDO::PARAM_STR);
+        $query->bindValue(:content, post->getContent(), PDO::PARAM_STR);
+        $query->bindValue(:pubish_date, date('Y/m/d H:i:s'), PDO::PARAM_STR);
+        $query->bindValue(:userID, post->getUserId(), PDO::PARAM_INT);
         return $query->execute();
     }
 
-    public function editPost()
+
+    public function editPost(): bool
     {
-        //TODO
+        $query = $this->pdo->prepare('UPDATE' . PDOFactory::DATABASE . '.posts SET title = :title, content = :content, userId = :userId WHERE id = :id)');
+        $query->bindValue(:title, post->getTitle(), PDO::PARAM_STR);
+        $query->bindValue(:content, post->getContent(), PDO::PARAM_STR);
+        $query->bindValue(:userID, post->getUserId(), PDO::PARAM_INT);
+        $query->bindValue(:id, post->getId(), PDO::PARAM_INT);
+        return $query->execute();
     }
-
-    */
-
-
 }
     
-
-
 
 $x = new PostManager();
 $x->getAllPosts();
