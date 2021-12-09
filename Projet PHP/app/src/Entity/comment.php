fda6a857356a8d3b1a34615fa2989f7e0ed1aa0e
ValentinR01 <vale.vr@icloud.com>
2021-12-09 15:33:31 +0100

    CommentManager

--- Projet PHP/app/src/Entity/comment.php
+++ Projet PHP/app/src/Entity/comment.php
@@ -8,86 +8,54 @@ class Comment extends BaseEntity
     private int $userId;
     private int $postId;
 
-    /**
-     * @return int
-     */
+
     public function getId(): int
     {
         return $this->id;
     }
 
-    /**
-     * @param int $id
-     */
     public function setId(int $id): void
     {
         $this->id = $id;
     }
 
-    /**
-     * @return string
-     */
     public function getContent(): string
     {
         return $this->content;
     }
 
-    /**
-     * @param string $content
-     */
     public function setContent(string $content): void
     {
         $this->content = $content;
     }
 
-    /**
-     * @return string
-     */
     public function getPublishedDate(): string
     {
         return $this->publishedDate;
     }
 
-    /**
-     * @param string $publishedDate
-     */
     public function setPublishedDate(string $publishedDate): void
     {
         $this->publishedDate = $publishedDate;
     }
 
-    /**
-     * @return int
-     */
     public function getUserId(): int
     {
         return $this->userId;
     }
 
-    /**
-     * @param int $userId
-     */
     public function setUserId(int $userId): void
     {
         $this->userId = $userId;
     }
 
-    /**
-     * @return int
-     */
     public function getPostId(): int
     {
         return $this->postId;
     }
 
-    /**
-     * @param int $postId
-     */
     public function setPostId(int $postId): void
     {
         $this->postId = $postId;
     }
-
-    
-
 }
--- Projet PHP/app/src/Manager/CommentManager.php
+++ Projet PHP/app/src/Manager/CommentManager.php
@@ -10,15 +10,15 @@ class CommentManager extends BaseManager {
 
     public function getAllCommentsFromPostId(int $postId): array
     {
-        $query = $this->pdo->query('SELECT * FROM ' .  PDOFactory::DATABASE . '.comments WHERE postId = :postId');
-        $query->bindValue(':postid', );
+        $query = $this->pdo->prepare('SELECT * FROM ' .  PDOFactory::DATABASE . '.comments WHERE postId = :postId');
+        $query->bindValue(':postId', $postId, PDO::PARAM_STR);
         $query->setFetchMode(\PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Entity\user');
         return $query->fetchAll();
     }
 
     public function createComment(comment $comment): bool
     {
-        $query = $this->pdo->query('INSERT INTO ' .  PDOFactory::DATABASE . '.comments (comment, publish_date, userId, postId) VALUES (:comment, publish_date, userId, postId)');
+        $query = $this->pdo->prepare('INSERT INTO ' .  PDOFactory::DATABASE . '.comments (comment, publish_date, userId, postId) VALUES (:comment, publish_date, userId, postId)');
         $query->bindValue(':comment', $comment->getComment(), PDO::PARAM_STR);
         $query->bindValue(':publish_date', $comment->getPublishDate(), PDO::PARAM_STR);
         $query->bindValue(':userId', $comment->getUserId(), PDO::PARAM_INT);
@@ -26,5 +26,21 @@ class CommentManager extends BaseManager {
         return $query->execute();
     }
 
-   
+    public function updateComment(comment $comment): bool
+    {
+        $query = $this->pdo->prepare('UPDATE ' .  PDOFactory::DATABASE . '.comments SET comment = :comment, publish_date = :publish_date, userId = :userId, postId = :postId WHERE id = :id');
+        $query->bindValue(':comment', $comment->getComment(), PDO::PARAM_STR);
+        $query->bindValue(':publish_date', $comment->getPublishDate(), PDO::PARAM_STR);
+        $query->bindValue(':userId', $comment->getUserId(), PDO::PARAM_INT);
+        $query->bindValue(':postId', $comment->getPostId(), PDO::PARAM_INT);
+        $query->bindValue(':id', $comment->getId(), PDO::PARAM_INT);
+        return $query->execute();
+    }
+
+    public function deleteComment(int $id): bool
+    {
+        $query = $this->pdo->prepare('DELETE ' . PDOFactory::DATABASE . '.comments WHERE id = :id');
+        $query->bindValue(':id', $id, PDO::PARAM_INT);
+        return $query->execute();
+    }
 }
