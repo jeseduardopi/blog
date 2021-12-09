92a8e245ac6bab498b09ce326d2e753e130d1b07
ValentinR01 <vale.vr@icloud.com>
2021-12-09 14:41:11 +0100

    hgg

--- Projet PHP/app/src/Manager/CommentManager.php
+++ Projet PHP/app/src/Manager/CommentManager.php
@@ -8,103 +8,23 @@ use App\Manager\BaseManager;
 
 class CommentManager extends BaseManager {
 
-
-    /**
-     * @return Comments[]
-     */
-    public function getAllComments(): array
+    public function getAllCommentsFromPostId(int $postId): array
     {
-        $query = 'SELECT * FROM ' . PDOFactory::DATABASE . '.comments';
-        //print_r($query);
-        //print_r($this->pdo);
-        $select = $this->pdo->query($query);
-
-        //print_r($select);
-        return $select->fetchAll(\PDO::FETCH_ASSOC);
-        //$return = [];
-
-        //$results = ['Testfvqdfq','test1qfsdfqsd','test2vwcxvwxc'];
-
-
-        //return  $results;
-        /*foreach ($results as $result) {
-            $className = $result['className'];
-            $return[] = new $className($comments);
-            return $return;
-        }*/
+        $query = $this->pdo->query('SELECT * FROM ' .  PDOFactory::DATABASE . '.comments WHERE postId = :postId');
+        $query->bindValue(':postid', );
+        $query->setFetchMode(\PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Entity\user');
+        return $query->fetchAll();
     }
 
-    public function addComment(Comments $personnage): Comments
+    public function createComment(comment $comment): bool
     {
-     /*   if (get_class($personnage) == 'App\Entity\Magicien') {
-            $query = 'INSERT INTO ' . PDOFactory::DATABASE . '.personnage (className, nom, pv, `force`, defense, sleep, lastSpell) VALUES (:className, :nom, :pv, :force, :defense, :sleep, :lastSpell)';
-        } else {
-            $query = 'INSERT INTO ' . PDOFactory::DATABASE . '.personnage (className, nom, pv, `force`, defense, sleep) VALUES (:className, :nom, :pv, :force, :defense, :sleep)';
-        }
-*/
-
-        $query = 'INSERT INTO ' . PDOFactory::DATABASE . '.personnage (className, nom, pv, `force`, defense, sleep, lastSpell) VALUES ( )';
-
-        $insert = $this->pdo->prepare($query);
-        $insert->bindValue(':', get_class($personnage), \PDO::PARAM_STR);
-        $insert->bindValue(':', $personnage->getNom(), \PDO::PARAM_STR);
-        $insert->bindValue(':', $personnage->getPv(), \PDO::PARAM_INT);
-        $insert->bindValue(':', $personnage->getForce(), \PDO::PARAM_INT);
-        $insert->bindValue(':', $personnage->getDefense(), \PDO::PARAM_INT);
-        $insert->bindValue(':', $personnage->getSleep(), \PDO::PARAM_STR);
-
-        if (get_class($personnage) == 'App\Entity\Magicien') {
-            $insert->bindValue(':lastSpell', $personnage->getLastSpell(), \PDO::PARAM_STR);
-        }
-
-        $insert->execute();
-
-        return $this->getPersonnageById($this->pdo->lastInsertId());
+        $query = $this->pdo->query('INSERT INTO ' .  PDOFactory::DATABASE . '.comments (comment, publish_date, userId, postId) VALUES (:comment, publish_date, userId, postId)');
+        $query->bindValue(':comment', $comment->getComment(), PDO::PARAM_STR);
+        $query->bindValue(':publish_date', $comment->getPublishDate(), PDO::PARAM_STR);
+        $query->bindValue(':userId', $comment->getUserId(), PDO::PARAM_INT);
+        $query->bindValue(':postId', $comment->getPostId(), PDO::PARAM_INT);
+        return $query->execute();
     }
 
-    public function getPersonnageById($id): Comments
-    {
-        $query = 'SELECT * FROM ' . PDOFactory::DATABASE . '.comments WHERE id = :id';
-        $select = $this->pdo->prepare($query);
-        $select->bindValue(':id', $id, \PDO::PARAM_INT);
-        $select->execute();
-
-        $result = $select->fetch(\PDO::FETCH_ASSOC);
-        $className = $result['className'];
-        return new $className($result);
-    }
-
-    public function updatePersonnage(Comments $comments): bool
-    {
-      /*  if (get_class($personnage) == 'App\Entity\Magicien') {
-            $query = 'UPDATE ' . PDOFactory::DATABASE . '.personnage SET pv = :pv, sleep = :sleep, lastSpell = :lastSpell WHERE id = :id';
-        } else {
-            $query = 'UPDATE ' . PDOFactory::DATABASE . '.personnage SET pv = :pv, sleep = :sleep WHERE id = :id';
-        }
-        $update = $this->pdo->prepare($query);
-        $update->bindValue(':id', $personnage->getId(), \PDO::PARAM_INT);
-        $update->bindValue(':pv', $personnage->getPv(), \PDO::PARAM_INT);
-        $update->bindValue(':sleep', $personnage->getSleep(), \PDO::PARAM_STR);
-
-        if (get_class($personnage) == 'App\Entity\Magicien') {
-            $update->bindValue(':lastSpell', $personnage->getLastSpell(), \PDO::PARAM_STR);
-        }
-      */
-
-        $query = 'UPDATE ' . PDOFactory::DATABASE . '.comments SET comment = :comment WHERE id = :id';
-
-        $update = $this->pdo->prepare($query);
-        $update->bindValue(':id', $comments->getId(), \PDO::PARAM_INT);
-        $update->bindValue(':pv', $comments->getPv(), \PDO::PARAM_INT);
-
-        return $update->execute();
-    }
-
-    public function deleteCommentById($id): bool
-    {
-        $query = 'DELETE FROM ' . PDOFactory::DATABASE . '.comments WHERE id = :id';
-        $select = $this->pdo->prepare($query);
-        $select->bindValue(':id', $id, \PDO::PARAM_INT);
-        return $select->execute();
-    }
+   
 }
