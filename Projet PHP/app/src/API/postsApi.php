1ad5c850f86f52af6bd38bb874f69819aad1a5b5
Eduardo Pina <80246060+jeseduardopi@users.noreply.github.com>
2021-12-10 14:54:54 +0100

    Delete Projet PHP directory

--- Projet PHP/app/src/API/postsApi.php
+++
@@ -1,9 +0,0 @@
-<?php
-use App\Manager\PostManager;
-use App\Manager\UserManager;
-
-
-$postManager = new PostManager();
-$userManager = new UserManager();
-
-
--- Projet PHP/app/src/View/Frontend/show.php
+++
@@ -1,4 +0,0 @@
-<h1><?= $test; ?>Page de post</h1>
-
-
-
